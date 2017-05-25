6308e791571dd104e729f359935ae1681e9aab59
Eitam <oeitam@gmail.com>
2017-05-25 22:38:21 +0300

    Second test

--- src/db/db.py
+++ src/db/db.py
@@ -3,6 +3,10 @@
 import sys
 import pandas
 
+class Db(object):
+    def __init__(self):
+        print("class Db initialized")
+
 
 
 
--- src/weekly.py
+++ src/weekly.py
@@ -1,1 +1,24 @@
+# oeitam
+
+import sys
+from src.db import db
+from src.gtd import gtd
+from src.server import server
+
+
+db = db.Db()
+
+
+
+
+
+
+
+
+
+
+
+
+# end
+
 
