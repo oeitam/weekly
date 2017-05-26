e02f43fa3427dd0769f18c5337b9543057d9f088
Eitam <oeitam@gmail.com>
2017-05-26 13:38:03 +0300

    Second test

--- src/gtd/gtd.py
+++ src/gtd/gtd.py
@@ -1,7 +1,8 @@
 
 
 class Gtd(object):
-    def __init__(self):
+    def __init__(self, db = None):
+        self.db = db
         print('class Gtd initialized')
 
 
--- src/weekly.py
+++ src/weekly.py
@@ -1,14 +1,16 @@
 # oeitam
 
 import sys
-from src.db import db
-from src.gtd import gtd
+from src.db import db as database
+from src.gtd import gtd as gettingthingsdone
 from src.server import server
+import logging
 
 
-db = db.Db()
-gtd = gtd.Gtd()
+db = database.Db()
+proc = gettingthingsdone.Gtd(db)
 server = server.Server()
+
 
 
 
